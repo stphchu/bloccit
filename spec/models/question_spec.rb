2323faceec9412aa1d70694bf3f24e5316210ae7
Stephanie <esceeaitchyu@gmail.com>
2018-06-25 19:03:39 -0700

    Add 'resolved' test to question_spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
 
   describe "attributes" do
     it "has a body attribute" do
-      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: false)
     end
   end
 end
