4bbd2085ae6d885affafbdb89e9666a3e17a8428
Stephanie <esceeaitchyu@gmail.com>
2018-06-29 15:56:44 -0700

    Complete Checkpoint 18 -- Rails: CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,31 @@ class PostsController < ApplicationController
   end
 
   def show
+  # #19
+    @post = Post.find(params[:id])
   end
 
   def new
+  # #7
+    @post = Post.new
+  end
+
+  def create
+ # #9
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+ # #10
+    if @post.save
+ # #11
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+     else
+ # #12
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,20 +17,63 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-# #10
-#  describe "GET show" do
- #    it "returns http success" do
- #      get :show
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+describe "GET show" do
+   it "returns http success" do
+# #16
+     get :show, params: { id: my_post.id }
+     expect(response).to have_http_status(:success)
+   end
+   it "renders the #show view" do
+# #17
+     get :show, params: { id: my_post.id }
+     expect(response).to render_template :show
+   end
+ 
+   it "assigns my_post to @post" do
+     get :show, params: { id: my_post.id }
+# #18
+     expect(assigns(:post)).to eq(my_post)
+   end
+end
 
- # describe "GET new" do
- #   it "returns http success" do
- #     get :new
- #     expect(response).to have_http_status(:success)
- #   end
- # end
+# #1
+    describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+ 
+ # #2
+      it "renders the #new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+ 
+ # #3
+      it "instantiates @post" do
+        get :new
+        expect(assigns(:post)).not_to be_nil
+      end
+    end
+ 
+    describe "POST create" do
+ # #4
+      it "increases the number of Post by 1" do
+        expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
+      end
+ 
+ # #5
+      it "assigns the new post to @post" do
+        post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+        expect(assigns(:post)).to eq Post.last
+      end
+ 
+ # #6
+      it "redirects to the new post" do
+        post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+        expect(response).to redirect_to Post.last
+      end
+    end
 
  #  describe "GET edit" do
  #    it "returns http success" do
