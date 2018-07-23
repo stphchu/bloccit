dd9be0276df929dd9aa7a430776bfcd1169f041a
Stephanie <esceeaitchyu@gmail.com>
2018-07-22 20:15:39 -0700

    Completion of Checkpoint 23 - Rails: Authentication - Signing Up

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+   let(:new_user_attributes) do
+     {
+         name: "BlocHead",
+         email: "blochead@bloc.io",
+         password: "blochead",
+         password_confirmation: "blochead"
+     }
+   end
+
+#NEW
+   describe "GET new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "instantiates a new user" do
+       get :new
+       expect(assigns(:user)).to_not be_nil
+     end
+   end
+
+#CREATE
+   describe "POST create" do
+     it "returns an http redirect" do
+       post :create, params: { user: new_user_attributes }
+       expect(response).to have_http_status(:redirect)
+     end
+ 
+     it "creates a new user" do
+       expect{
+         post :create, params: { user: new_user_attributes }
+       }.to change(User, :count).by(1)
+     end
+ 
+     it "sets user name properly" do
+       post :create, params: { user: new_user_attributes }
+       expect(assigns(:user).name).to eq new_user_attributes[:name]
+     end
+ 
+     it "sets user email properly" do
+       post :create, params: { user: new_user_attributes }
+       expect(assigns(:user).email).to eq new_user_attributes[:email]
+     end
+ 
+     it "sets user password properly" do
+       post :create, params: { user: new_user_attributes }
+       expect(assigns(:user).password).to eq new_user_attributes[:password]
+     end
+ 
+     it "sets user password_confirmation properly" do
+       post :create, params: { user: new_user_attributes }
+       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+     end
+   end
+end
