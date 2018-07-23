4b5a0decc55181c7d89fd622182ceb029765fe79
Stephanie <esceeaitchyu@gmail.com>
2018-07-22 18:17:40 -0700

    Add 'format_name' to capitalize the user's name before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
@@ -10,4 +11,15 @@ class User < ApplicationRecord
              length: { minimum: 3, maximum: 254 }
 
    has_secure_password
+
+ def format_name
+    if name
+      name_array = []
+      name.split(" ").each do |n| 
+ 	name_array.push(n.capitalize)
+      end
+      self.name = name_array.join(" ")
+    end
+ end
+
 end
