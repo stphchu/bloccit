6321412da3e66ee6c97d2c9534d81ccb17c7deea
Stephanie <esceeaitchyu@gmail.com>
2018-07-24 22:02:55 -0700

    Add user confirmation page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  
+
+ # ... CRUD methods
+ # Rails 5.1.4+ require you to specify the parameters you want to pass through.
+ def user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+
+ # This makes `user_params()` available in the view.
+   helper_method :user_params
+
 #NEW 
    def new
      @user = User.new
@@ -14,7 +23,7 @@ class UsersController < ApplicationController
      @user.password_confirmation = params[:user][:password_confirmation]
  
      if @user.save
-       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+       flash[:notice] = "Welcome to Bloccit, #{@user.name}!"
        redirect_to root_path
      else
        flash.now[:alert] = "There was an error creating your account. Please try again."
@@ -22,4 +31,12 @@ class UsersController < ApplicationController
      end
    end
 
+#CONFIRM
+   def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
    end
 
    resources :users, only: [:new, :create]
+   post 'users/confirm' => 'users#confirm'
 
    get 'about' => 'welcome#about'
 
