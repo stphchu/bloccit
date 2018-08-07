c895bd577d0ad9974a21d32be52019d6124dfc4a
Stephanie <esceeaitchyu@gmail.com>
2018-08-07 12:04:48 -0700

    Add mailer for new post creation (rev1)

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
  
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+  def new_post(user, post)
+     headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     
+     @user = user
+     @post = post
+
+     mail(to: user.email, subject: "You're following, #{post.title}")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
    has_many :favorites, dependent: :destroy
+ 
+   after_create :create_favorite   
 
    default_scope { order('rank DESC') }
 
@@ -29,4 +31,14 @@ class Post < ApplicationRecord
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end
+
+private
+   def create_favorite
+     Favorite.create(post: self, user: self.user)
+     FavoriteMailer.new_post(self).deliver_now
+
+    @user = user
+
+   end
+
 end
